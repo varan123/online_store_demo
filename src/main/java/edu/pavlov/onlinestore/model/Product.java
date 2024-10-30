4117bfe0b4378f37e25a466b5de2524c4cb0dbf9
Michael Pavlov <34164390+varan123@users.noreply.github.com>
2024-10-30 15:17:05 -0400

    add unit testing

---
+++ src/main/java/edu/pavlov/onlinestore/model/Product.java
@@ -0,0 +1,10 @@
+package edu.pavlov.onlinestore.model;
+
+import lombok.Data;
+
+@Data
+public class Product  {
+    private Long id;
+    private String name;
+    private String description;
+}
---
+++ src/test/java/edu/pavlov/onlinestore/tests/VoidTest.java
@@ -0,0 +1,10 @@
+package edu.pavlov.onlinestore.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class VoidTest {
+    @Test
+    public void test() {
+        System.out.println("123");
+    }
+}
