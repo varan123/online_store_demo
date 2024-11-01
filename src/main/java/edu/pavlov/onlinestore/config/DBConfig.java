6651026e340780010246dcde07494e4eddfa5c71
Michael Pavlov <34164390+varan123@users.noreply.github.com>
2024-11-01 12:04:17 -0400

    db added

---
+++ src/main/java/edu/pavlov/onlinestore/config/DBConfig.java
@@ -0,0 +1,36 @@
+package edu.pavlov.onlinestore.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DBConfig {
+
+    @Value("${spring.datasource.url}")
+    String url;
+
+    @Value("${spring.datasource.username}")
+    String username;
+
+    @Value("${spring.datasource.password}")
+    String password;
+
+    @Value("${spring.datasource.driverClassName}")
+    String driver;
+
+    @Bean
+    public DataSource dataSource()
+    {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(driver);
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+        return dataSourceBuilder.build();
+    }
+
+}
