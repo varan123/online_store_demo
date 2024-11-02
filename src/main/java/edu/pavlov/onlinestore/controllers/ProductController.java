fccdf65aa1e1ba015caa1ae4fc9f2cffbec3f730
Michael Pavlov <34164390+varan123@users.noreply.github.com>
2024-11-01 23:47:23 -0400

    add dao and testing with DB

--- src/main/java/edu/pavlov/onlinestore/controllers/ProductController.java
+++ src/main/java/edu/pavlov/onlinestore/controllers/ProductController.java
@@ -1,7 +1,9 @@
 package edu.pavlov.onlinestore.controllers;
 
+import edu.pavlov.onlinestore.dao.ProductsDAO;
 import edu.pavlov.onlinestore.model.Product;
 import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ProductController  {
 
     /**
+     * The productsDAO field is used to get a product by its id.
+     */
+    @Autowired
+    private ProductsDAO productsDAO;
+
+    /**
      * The getProduct method returns a product by its id.
      * @param id The product's id.
      * @return String
@@ -19,6 +27,6 @@ public class ProductController  {
     @GetMapping
     public Product getProduct(
             @Parameter(description = "The product's id") final Long id) {
-        return new Product();
+        return productsDAO.getProductById(id);
     }
 }
---
+++ src/main/java/edu/pavlov/onlinestore/dao/ProductsDAO.java
@@ -0,0 +1,47 @@
+package edu.pavlov.onlinestore.dao;
+
+import edu.pavlov.onlinestore.model.Product;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+@Component
+public class ProductsDAO {
+
+    /**
+     * The dataSource field is used to get a connection to the database.
+     */
+    @Autowired
+    private DataSource dataSource;
+
+    /**
+     * The getProductById method returns a product by its id.
+     * @param id The product's id.
+     * @return Product
+     */
+    @SneakyThrows
+    public Product getProductById(final Long id) {
+        Product product = null;
+
+        try (Connection connection = dataSource.getConnection()) {
+                PreparedStatement statement = connection.prepareStatement(
+                                "SELECT name, description FROM product "
+                                        + "WHERE id = " + id);
+                statement.execute();
+            ResultSet resultSet = statement.getResultSet();
+            if (resultSet.next()) {
+                product = new Product();
+                product.setName(resultSet.getString("name"));
+                product.setDescription(resultSet.getString("description"));
+            }
+        }
+
+        return product;
+    }
+}
